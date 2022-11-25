5e2e4ae80eb0a55a78f95d85ca6b745ccc0a6873
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2022-11-25 16:19:31 -0600

    Nuevo repositorio

---
+++ pages/list.jsx
@@ -0,0 +1,216 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
+import MainHead from '../components/MainHead'
+
+
+export default function List(){
+    return(
+        <div className={styles.container}>
+
+            <MainHead tituloPestana="Menú Principal"/>
+            <Header/>
+            
+            <center>
+            <form className={styles.buscarForm}>
+            <input className={styles.barraBusqueda} placeholder="Nombre, empresa, correo, etc."></input>
+            <button className={styles.btnBuscar}><font face="Work Sans">BUSCAR</font></button>
+            </form>
+            </center>
+            
+            <div className={styles.cont}>
+
+                <div className={styles.tablaCont}>
+
+                    <div className={styles.headTabla}>
+                        Nombre(s) | Paterno | Materno | Empressa | Email | Teléfono | Cel
+                    </div>
+                    
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    
+                    
+                    
+                    
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
