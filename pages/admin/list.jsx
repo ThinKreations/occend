207c69e207f1ac91db0a048c896da959d67a8af7
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2022-11-25 16:42:26 -0600

    ?

--- pages/admin/list.jsx
+++ pages/admin/list.jsx
@@ -11,40 +11,48 @@ export default function List(){
         <MainHead tituloPestana="Menú Principal"/>
         <div className={styles.container}>
 
-                        
-                <Header/>
-
-                <center>
-                <form className={styles.buscarForm}>
-                <input className={styles.barraBusqueda} placeholder="Nombre, empresa, correo, etc."></input>
-                <button className={styles.btnBuscar}><font face="Work Sans">BUSCAR</font></button>
-                </form>
-                </center>
-
-                <div className={styles.cont}>
-
+            <MainHead tituloPestana="Menú Principal"/>
+            <Header/>
+            
+            <center>
+            <form className={styles.buscarForm}>
+            <input className={styles.barraBusqueda} placeholder="Nombre, empresa, correo, etc."></input>
+            <button className={styles.btnBuscar}><font face="Work Sans">BUSCAR</font></button>
+            </form>
+            </center>
+            <center><button className={styles.addCliente} onClick={()=>router.push('agregarCliente')}>Agregar Cliente</button></center>
+            <div className={styles.cont}>
+                
                 <div className={styles.tablaCont}>
 
-                        <div className={styles.headTabla}>
-                        Nombre(s) | Paterno | Materno | Empressa | Email | Teléfono | Cel
-                        </div>
-                        
-                        <div className={styles.clienteC}>                
-                                <div className={styles.datoCliente}>
-                                Juan Martín<b> | </b>
-                                Nápoles<b> | </b>
-                                Munguía<b> | </b> 
-                                OCCEND<b> | </b>
-                                correo@occend.com.mx<b> | </b> 
-                                1234567890<b> | </b> 
-                                0987654321
-                                </div>
-                        </div>                   
-
+                    <div className={styles.headTabla}>
+                        Nombre(s) | Paterno | Materno | Empresa | Dirección | Ciudad | CP | RFC | Email | Teléfono | Cel
+                    </div>
+                    
+                    <div className={styles.clienteC}>                
+                            <div className={styles.datoCliente}>
+                            Juan Martín<b> | </b>
+                            Nápoles<b> | </b>
+                            Munguía<b> | </b> 
+                            OCCEND<b> | </b>
+                            Dirección<b> | </b>
+                            CDMX<b> | </b>
+                            00000<b> | </b>
+                            RFC1234567890<b> | </b>
+                            correo@occend.com.mx<b> | </b> 
+                            1234567890<b> | </b> 
+                            0987654321
+                            </div>
+                    </div>
+                    
+                    
+                    
+                    
                 </div>
 
-                </div>
-                </div>
+
+            </div>
+        </div>
         </>
         
     )
