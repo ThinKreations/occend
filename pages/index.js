2658542c8c60e105761483a7a4010935b50426ff
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2022-12-01 17:14:35 -0600

    ya le estoy agarrando la onda a esto

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,59 @@
 import Head from 'next/head'
-import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
-
-
-import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup'
 import { useRouter } from 'next/router'
+import { schemaIniciarSesion } from '../../schemas/iniciarSesion'
+import swal from 'sweetalert'
 
 export default function Home() {
-  const router = useRouter()
+
+  const Router = useRouter()
+
+const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schemaIniciarSesion)
+  })
+
+  const onSubmit = async data => {
+    const res = await fetch(`localhost:8080/login/`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        correo: data.correo,
+        password: data.password
+      })
+    })
+    const resJSON = await res.json()
+    if (res.status !== 200) {
+      swal({
+        title: 'Error al iniciar sesion.',
+        text: resJSON.msg,
+        icon: 'error',
+        button: 'Ok'
+      })
+    } else {
+      swal({
+        title: 'Finalizado.',
+        text: resJSON.msg,
+        icon: 'success',
+        button: 'Ok',
+        timer: '3000'
+      })
+      localStorage.setItem('id', resJSON.id);
+      localStorage.setItem('token', resJSON.token)
+      Router.push('/cliente/list')
+    }
+  }
+
 
   return (
     <>
@@ -33,7 +78,7 @@ export default function Home() {
                 <input className={styles.inputLogin} type={'email'} placeholder="Correo"></input><br/>
                 <input className={styles.inputLogin} type="password" placeholder="Contraseña"></input><br/>
                 <center>
-                  <button className={styles.btnLog} onClick={()=>router.push('./cliente/list')}>Acceder</button>
+                  <button className={styles.btnLog}>Acceder</button>
                   
                   
                 </center>
