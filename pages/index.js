e1e5cc3fc371b5574b486c1095d42cb9c481acaa
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2022-12-05 13:40:11 -0600

    ya?

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,62 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
-
+import swal from 'sweetalert'
+import { yupResolver } from '../node_modules/@hookform/resolvers/yup'
 
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useForm } from 'react-hook-form'
+
+import { schemaIniciarSesion } from '../schemas/iniciarSesion'
 
 export default function Home() {
   const router = useRouter()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(schemaIniciarSesion)
+  })
+
+  const onSubmit = async data => {
+    const res = await fetch(`localhost:8080/login`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        correo: data.correo,
+        password: data.password
+      })
+    })
+    const resJSON = await res.json()
+    if (res.status !== 200) {
+      swal({
+        title: 'Error al iniciar sesion.',
+        text: resJSON.msg,
+        icon: 'error',
+        button: 'Ok'
+      })
+    } else {
+      swal({
+        title: 'Finalizado.',
+        text: resJSON.msg,
+        icon: 'success',
+        button: 'Ok',
+        timer: '3000'
+      })
+      localStorage.setItem('id', resJSON.id);
+      localStorage.setItem('token', resJSON.token)
+      router.push('./cliente/')
+    }
+  }
+
+
+
 
   return (
     <>
@@ -26,14 +75,14 @@ export default function Home() {
 
           <form className={styles.loginForm}>
                 
-                <font face="Work Sans">
+                <font face="Work Sans" onSubmit={handleSubmit(onSubmit)}>
                 
                 <center><h1>Iniciar Sesión</h1></center>
-
-                <input className={styles.inputLogin} type={'email'} placeholder="Correo"></input><br/>
-                <input className={styles.inputLogin} type="password" placeholder="Contraseña"></input><br/>
+                <input {...register('correo')} className={styles.inputLogin} placeholder='Correo'/>
+                <input {...register('password')} className={styles.inputLogin} placeholder='Contraseña' type="password"/>
+                
                 <center>
-                  <button className={styles.btnLog} onClick={()=>router.push('./cliente/list')}>Acceder</button>
+                  <button className={styles.btnLog} type='submit'>Acceder</button>
                   
                   
                 </center>
